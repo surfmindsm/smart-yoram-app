39f7b7ecd78968cfc1a51b47a140deb375f87034
surfmindsm <surfmind.sm@gmail.com>
2025-07-30 15:35:50 +0900

    Initial commit: Flutter app with Supabase integration

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://adzhdsajdamrflvybhxq.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkemhkc2FqZGFtcmZsdnliaHhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NDg5ODEsImV4cCI6MjA2OTQyNDk4MX0.pgn6M5_ihDFt3ojQmCoc3Qf8pc7LzRvQEIDT7g1nW3c';
+}
