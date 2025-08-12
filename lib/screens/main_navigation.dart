3098b4c3a735a47207d8aee9b53d30363d1ada13
surfmindsm <surfmind.sm@gmail.com>
2025-08-12 21:02:45 +0900

    11 완료

--- lib/screens/main_navigation.dart
+++ lib/screens/main_navigation.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_yoram_app/resource/color_style.dart';
+import 'package:smart_yoram_app/resource/text_style.dart';
 import 'home_screen.dart';
 
 import 'bulletin_screen.dart';
@@ -25,29 +28,6 @@ class _MainNavigationState extends State<MainNavigation> {
     const SettingsScreen(),
   ];
 
-  final List<BottomNavigationBarItem> _navItems = [
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      label: '홈',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.contacts),
-      label: '주소록',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.book),
-      label: '주보',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.announcement),
-      label: '교회소식',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.settings),
-      label: '설정',
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,17 +36,107 @@ class _MainNavigationState extends State<MainNavigation> {
         index: _currentIndex,
         children: _screens,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        selectedItemColor: Colors.blue[700],
-        unselectedItemColor: Colors.grey,
-        items: _navItems,
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFFFFFFF),
+        ),
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).padding.bottom,
+        ),
+        child: Container(
+          height: 52.h,
+          child: Row(
+            children: [
+              _NavItem(
+                iconPath: 'assets/icons/icon_home.svg',
+                label: '홈',
+                isActive: _currentIndex == 0,
+                onTap: () => _onTap(0),
+              ),
+              _NavItem(
+                iconPath: 'assets/icons/icon_member.svg',
+                label: '주소록',
+                isActive: _currentIndex == 1,
+                onTap: () => _onTap(1),
+              ),
+              _NavItem(
+                iconPath: 'assets/icons/icon_bulletin.svg',
+                label: '주보',
+                isActive: _currentIndex == 2,
+                onTap: () => _onTap(2),
+              ),
+              _NavItem(
+                iconPath: 'assets/icons/icon_notice.svg',
+                label: '교회소식',
+                isActive: _currentIndex == 3,
+                onTap: () => _onTap(3),
+              ),
+              _NavItem(
+                iconPath: 'assets/icons/icon_setting.svg',
+                label: '설정',
+                isActive: _currentIndex == 4,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTap(int index) {
+    if (_currentIndex == index) return;
+    setState(() => _currentIndex = index);
+  }
+}
+
+class _NavItem extends StatelessWidget {
+  const _NavItem({
+    required this.iconPath,
+    required this.label,
+    required this.isActive,
+    required this.onTap,
+  });
+
+  final String iconPath;
+  final String label;
+  final bool isActive;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final iconColor = isActive ? AppColor.secondary06 : AppColor.secondary04;
+    final textColor = isActive ? AppColor.secondary06 : AppColor.secondary04;
+    
+    return Expanded(
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: onTap,
+        child: Container(
+          height: 52,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                iconPath,
+                width: 24.w,
+                height: 24.h,
+                colorFilter: ColorFilter.mode(
+                  iconColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                label,
+                style: AppTextStyle(
+                  color: textColor,
+                ).c2(),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
